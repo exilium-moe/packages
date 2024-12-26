12fd39edb592c9ddb153042f54d70aa8939de697
Luzefiru <105530193+Luzefiru@users.noreply.github.com>
2024-12-26 22:07:47 +0800

    fix: coerce numbers from KV

--- gacha/src/types/TotalPullsByItemPerDay.ts
+++ gacha/src/types/TotalPullsByItemPerDay.ts
@@ -1,11 +1,13 @@
-import { z } from "zod"
+import { z } from 'zod';
 
 export const TotalPullsByItemPerDaySchema = z.array(
   z.object({
     day: z.string().min(4),
-    item: z.number().gte(1),
-    count: z.number().gte(0),
-  }),
-)
+    item: z.coerce.number().gte(1),
+    count: z.coerce.number().gte(0),
+  })
+);
 
-export type TotalPullsByItemPerDay = z.infer<typeof TotalPullsByItemPerDaySchema>
+export type TotalPullsByItemPerDay = z.infer<
+  typeof TotalPullsByItemPerDaySchema
+>;
--- gacha/src/types/TotalPullsPerPityAndRarity.ts
+++ gacha/src/types/TotalPullsPerPityAndRarity.ts
@@ -1,19 +1,21 @@
-import { z } from "zod"
-import { RarityEnumSchema } from "@exilium-moe/gacha/types/RarityEnum"
+import { z } from 'zod';
+import { RarityEnumSchema } from '@exilium-moe/gacha/types/RarityEnum';
 
 export const TotalPullsPerPityAndRaritySchema = z.array(
   z.union([
     z.object({
       rarity: z.literal(RarityEnumSchema.Values.SR),
-      pity: z.number().gte(1).lte(10, { message: "SR pity cannot go beyond 10" }),
-      count: z.number().gte(0),
+      pity: z.number().gte(0),
+      count: z.coerce.number().gte(0),
     }),
     z.object({
       rarity: z.literal(RarityEnumSchema.Values.SSR),
-      pity: z.number().gte(1).lte(80, { message: "SSR pity cannot go beyond 80" }),
-      count: z.number().gte(0),
+      pity: z.number().gte(0),
+      count: z.coerce.number().gte(0),
     }),
-  ]),
-)
+  ])
+);
 
-export type TotalPullsPerPityAndRarity = z.infer<typeof TotalPullsPerPityAndRaritySchema>
+export type TotalPullsPerPityAndRarity = z.infer<
+  typeof TotalPullsPerPityAndRaritySchema
+>;
